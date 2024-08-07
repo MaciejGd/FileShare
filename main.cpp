9041aa0ccd988d25224445a8face832903147e65
MaciejGd <maciekzgk@gmail.com>
2024-08-07 22:10:50 +0200

    Simple files downloading works

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ void createJSON(const std::vector<std::string>& files)
     exit(1);
   }
   json_file << "{\n";
-  json_file << "\"options\" : [";
+  json_file << "\"files\" : [";
   for (int i = 0; i < files.size(); i++)
   {
     json_file << "{\"url\":\"" << files[i] << "\"," << "\"name\":\""<< getFileName(files[i]) << "\"}";
@@ -65,18 +65,18 @@ void createJSON(const std::vector<std::string>& files)
 
 int main(int argc, const char** argv)
 {
-  // if (argc == 1)
-  // {
-  //   http::TcpServer server = http::TcpServer("0.0.0.0", 8080);
-  // }
-  // else if (argc == 2)
-  // {
-  //   http::TcpServer server = http::TcpServer("0.0.0.0", 8080, std::string(argv[1]));
-  // }
-  // else {
-  //   std::cout << "Too much input arguments, terminating program execution...\n";
-  // }
-  //testing of new mechanism
   createJSON(prepareArgsJSON());  
+  if (argc == 1)
+  {
+    http::TcpServer server = http::TcpServer("0.0.0.0", 8080);
+  }
+  else if (argc == 2)
+  {
+    http::TcpServer server = http::TcpServer("0.0.0.0", 8080, std::string(argv[1]));
+  }
+  else {
+    std::cout << "Too much input arguments, terminating program execution...\n";
+  }
+  
   return 0;
 }
